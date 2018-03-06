6784d8f10e8e3f4a060b867d02e7f8666a97db7d
Dmitry Lavnikevich <1469488+githaff@users.noreply.github.com>
2018-03-06 22:48:52 +0300

    Make forward/backward mouse buttons be the move/resize window

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
 # This is test libusb project
 
 CFLAGS = -Wall -pedantic
-CFLAGS += `pkg-config --cflags libusb-1.0`
-LDFLAGS += `pkg-config --libs libusb-1.0`
-
-xusb : xusb.c
-	gcc $(CFLAGS) $(LDFLAGS) -o xusb xusb.c
+CFLAGS += `pkg-config --cflags hidapi-libusb` -g
+LDFLAGS += `pkg-config --libs hidapi-libusb`
 
 usb : main.o usb.o aux.o
 	gcc $(LDFLAGS) main.o usb.o aux.o -o usb
+
+xusb : xusb.c
+	gcc -Wall -pedantic `pkg-config --cflags --libs libusb-1.0` -o xusb xusb.c
 
 main.o : main.c
 	gcc $(CFLAGS) -c main.c
@@ -22,6 +22,6 @@ aux.o : aux.c
 
 clean :
 	rm -f *.o
-	rm usb
+	rm -f usb xusb
 
 .PHONY : clean
--- main.c
+++ main.c
@@ -1,4 +1,8 @@
-#include <hidapi.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "aux.h"
 
@@ -8,60 +12,107 @@ int main()
 	int ret = 0;
 	unsigned char buf[256];
 	int buf_len = sizeof(buf) / sizeof(buf[0]);
+    int fd;
+    int len;
 	int i;
+    /* size_t cmd_short_size = 7; */
+    size_t cmd_long_size = 20;
 
-	hid_device *handle;
-	struct hid_device_info *devs, *cur_dev;
 
 	msg("Starting");
 
-	devs = hid_enumerate(0x046d, 0xc52b);
-	cur_dev = devs;
-	while (cur_dev) {
-		printf("Device Found\n  type: %04hx %04hx\n  path: %s\n  serial_number: %ls",
-			cur_dev->vendor_id, cur_dev->product_id, cur_dev->path, cur_dev->serial_number);
-		printf("\n");
-		printf("  Manufacturer: %ls\n", cur_dev->manufacturer_string);
-		printf("  Product:      %ls\n", cur_dev->product_string);
-		printf("\n");
-		cur_dev = cur_dev->next;
-	}
-	hid_free_enumeration(devs);
-	
-	handle = hid_open(0x046d, 0xc52b, NULL);
-	if (!handle)
-		crit("No device found");
+    fd = open("/dev/hidraw0", O_RDWR);
+    if (fd < 0) {
+        perror("Can't open");
+        return 1;
+    }
 
-	unsigned char data_send[] = {
-		0x0,
-	};
-	int len_send = sizeof(data_send) / sizeof(data_send[0]);
-	printf("Sending data of size %d\n", len_send);
+    i = 0;
 
-//	int len = hid_write(handle, data_send, len_send);
-//	int len = hid_send_feature_report(handle, data_send, len_send);
-	int len = hid_write(handle, data_send, len_send);
-	len = hid_get_feature_report(handle, data_send, len_send);
-	printf("[%d] - received\n", len);
-	for (i = 0; i < len; i++) {
-		msgn(" %02d, data_send[i]");
-	}
+    /* uint8_t cmd_smart_shift_7[] = { */
+    /*     0x10,   // size - 7 *\/ */
+    /*     0x02,   // device - 2 *\/ */
+    /*     0x0b,   // index of the feature *\/ */
+    /*     0x10,   // function (0xU_) *\/ */
+    /*     0x02,   // function *\/ */
+    /*     0x07,   // value *\/ */
+    /*     0x07, */
+    /* }; */
+    /* uint8_t cmd_thumb_event[] = { */
+    /*     0x11, 0x02, 0x08, 0x3a, 0x00, 0xc3, 0x03, 0x00, */
+    /*     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, */
+    /*     0x00, 0x00, 0x00, 0x00 */
+    /* }; */
 
-	printf("===================================\n");
-	
-	for (i = 0; i < 100; i++) {
-		int j;
-		int len;
+    uint8_t cmd_fw_event[] = {
+        0x11, 0x02, 0x08, 0x3c, 0x00, 0x56, 0x03, 0x00,
+        0x51, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00
+    };
+    uint8_t cmd_bk_event[] = {
+        0x11, 0x02, 0x08, 0x3c, 0x00, 0x53, 0x03, 0x00,
+        0x50, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00
+    };
 
-		len = hid_read_timeout(handle, buf, buf_len, 1000);
+    len = write(fd, cmd_fw_event, cmd_long_size);
+    printf("Written: %d/%d\n", len, i);
+    len = write(fd, cmd_bk_event, cmd_long_size);
+    printf("Written: %d/%d\n", len, i);
 
-		msgn("[%d]:", len);
+    int cnt = 0;
+    unsigned char int_fw_down[] = {
+        0x11, 0x02, 0x08, 0x00, 0x00, 0x56, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00 };
+    unsigned char int_bk_down[] = {
+        0x11, 0x02, 0x08, 0x00, 0x00, 0x53, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00 };
+    /* unsigned char int_thumb_down[] = { */
+    /*     0x11, 0x02, 0x08, 0x00, 0x00, 0xc3, 0x00, 0x00, */
+    /*     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, */
+    /*     0x00, 0x00, 0x00, 0x00 }; */
+    unsigned char int_up[] = {
+        0x11, 0x02, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00 };
 
-		for (j = 0; j < len; j++) {
-			msgn(" %02x", buf[j]);
-		}
-		msg("");
-	}
+    int button_stat = 0;
+    while (1) {
+        cnt++;
+
+        len = read(fd, buf, buf_len);
+        if (len < 0) {
+            perror("Can't read");
+            return 1;
+        }
+
+        printf("[%2d]: ", len);
+        for (i = 0; i < len - 1; i++)
+            printf("0x%02x ", buf[i]);
+        if (len > 0)
+            printf("0x%02x\n", buf[i]);
+
+        if (len == cmd_long_size) {
+            if (button_stat != 0 && !memcmp(buf, int_up, cmd_long_size)) {
+                system("xdotool keyup 'Hyper_L'");
+                system("xdotool mouseup 1");
+                system("xdotool mouseup 3");
+                button_stat = 0;
+            }
+            if (button_stat != 1 && !memcmp(buf, int_fw_down, cmd_long_size)) {
+                system("xdotool keydown 'Hyper_L'");
+                system("xdotool mousedown 1");
+                button_stat = 1;
+            }
+            if (button_stat != 2 && !memcmp(buf, int_bk_down, cmd_long_size)) {
+                system("xdotool keydown 'Hyper_L'");
+                system("xdotool mousedown 3");
+                button_stat = 2;
+            }
+        }
+    }
 
 	return ret;
 }
