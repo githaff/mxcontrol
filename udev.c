321e0d8ce1c43c9697836599abdf398ab36f33a2
Dmitry Lavnikevich <1469488+githaff@users.noreply.github.com>
2018-07-03 17:59:50 +0300

    Implement udev monitoring for MX Master devices

--- udev.c
+++ udev.c
@@ -1,6 +1,3 @@
-#if 1
-
-
 #include <libudev.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -51,6 +48,29 @@ struct udev_device *find_hidraw(struct udev *udev, struct udev_device *dev_ps)
     return dev_hidraw;
 }
 
+
+/* Check if the device received from monitor is the expected monitor action
+ */
+bool is_expected(struct udev_device *dev)
+{
+    const char *action;
+    action = udev_device_get_action(dev);
+    if (!action)
+        return false;
+    if (strcmp(action, "change") &&
+        strcmp(action, "add"))
+        return false;
+
+    const char *online;
+    online = udev_device_get_sysattr_value(dev, "online");
+    if (!online)
+        return false;
+    if (strcmp(online, "1"))
+        return false;
+
+    return true;
+}
+
 /* Check if udev device is actually a power_supply related to the MC Master mouse
  */
 bool is_mxm_ps(struct udev_device *dev)
@@ -91,26 +111,91 @@ bool is_mxm_ps(struct udev_device *dev)
     return false;
 }
 
-
-/* TODO: memory leakage analyze */
-
-int main (void)
+/* Setup MX Master device
+ */
+bool setup_mxm(struct udev_device *dev_hidraw)
 {
-    struct udev *udev;
+    const char *devnode;
+
+    devnode = udev_device_get_devnode(dev_hidraw);
+    printf("Performing setup on '%s'\n", devnode);
+    /* TODO: implement setup */
+
+    return true;
+}
+
+/* Monitor loop over udev events. Run MX Master setup each time MX Master needs
+   to re-set configuration. E.g. connected to the host or MX Master itself is
+   switched from another connection to this one
+*/
+bool monitor(struct udev *udev)
+{
+   	struct udev_monitor *mon;
+    int fd_mon;
+
+    mon = udev_monitor_new_from_netlink(udev, "udev");
+    if (!mon) {
+        printf("Error: failed to create udev monitor\n");
+        return false;
+    }
+    udev_monitor_filter_add_match_subsystem_devtype(mon, "power_supply", NULL);
+    udev_monitor_enable_receiving(mon);
+    fd_mon = udev_monitor_get_fd(mon);
+
+    printf("Entering monitor loop...\n");
+    while (1) {
+        fd_set fds;
+        int ret;
+        FD_ZERO(&fds);
+        FD_SET(fd_mon, &fds);
+        ret = select(fd_mon + 1, &fds, NULL, NULL, NULL);
+
+        if (ret > 0 && FD_ISSET(fd_mon, &fds)) {
+            struct udev_device *dev_hidraw;
+            struct udev_device *dev_mon;
+
+            dev_mon = udev_monitor_receive_device(mon);
+
+            if (!is_expected(dev_mon)) {
+                udev_device_unref(dev_mon);
+                continue;
+            }
+
+            if (!is_mxm_ps(dev_mon)) {
+                udev_device_unref(dev_mon);
+                continue;
+            }
+
+            dev_hidraw = find_hidraw(udev, dev_mon);
+            if (!dev_hidraw) {
+                udev_device_unref(dev_mon);
+                continue;
+            }
+
+            setup_mxm(dev_hidraw);
+
+            udev_device_unref(dev_mon);
+            udev_device_unref(dev_hidraw);
+        }
+    }
+
+    return true;
+}
+
+/* Scan for all present MX Master devices
+ */
+bool scan_for_mxm(struct udev *udev)
+{
     struct udev_enumerate *enumerate;
     struct udev_list_entry *devices, *dev_list_entry;
-
-    /* Create the udev object */
-    udev = udev_new();
-    if (!udev) {
-        printf("Can't create udev\n");
-        exit(1);
-    }
+    bool found = false;
 
     enumerate = udev_enumerate_new(udev);
     udev_enumerate_add_match_subsystem(enumerate, "power_supply");
     udev_enumerate_scan_devices(enumerate);
     devices = udev_enumerate_get_list_entry(enumerate);
+
+    printf("Scanning current MX Master devices...\n");
 
     udev_list_entry_foreach(dev_list_entry, devices) {
         const char *path;
@@ -131,6 +216,7 @@ int main (void)
             continue;
         }
 
+        found = true;
         printf("DEVICE FOUND:\n");
         printf("* subsystem: %s\n", udev_device_get_subsystem(dev_hidraw));
         printf("* syspath: %s\n", udev_device_get_syspath(dev_hidraw));
@@ -143,52 +229,14 @@ int main (void)
     /* Free the enumerator object */
     udev_enumerate_unref(enumerate);
 
-    udev_unref(udev);
-
-    return 0;
+    return found;
 }
-#else
 
-/*******************************************
- libudev example.
-
- This example prints out properties of
- each of the hidraw devices. It then
- creates a monitor which will report when
- hidraw devices are connected or removed
- from the system.
-
- This code is meant to be a teaching
- resource. It can be used for anyone for
- any reason, including embedding into
- a commercial product.
-
- The document describing this file, and
- updated versions can be found at:
-    http://www.signal11.us/oss/udev/
-
- Alan Ott
- Signal 11 Software
- 2010-05-22 - Initial Revision
- 2010-05-27 - Monitoring initializaion
-              moved to before enumeration.
-*******************************************/
-
-#include <libudev.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
-#include <unistd.h>
+/* TODO: memory leakage analyze */
 
 int main (void)
 {
     struct udev *udev;
-    struct udev_enumerate *enumerate;
-     struct udev_list_entry *devices, *dev_list_entry;
-    struct udev_device *dev;
-
-   	struct udev_monitor *mon;
-    int fd;
 
     /* Create the udev object */
     udev = udev_new();
@@ -197,150 +245,12 @@ int main (void)
         exit(1);
     }
 
-    /* This section sets up a monitor which will report events when
-       devices attached to the system change.  Events include "add",
-       "remove", "change", "online", and "offline".
+    monitor(udev);
 
-       This section sets up and starts the monitoring. Events are
-       polled for (and delivered) later in the file.
-
-       It is important that the monitor be set up before the call to
-       udev_enumerate_scan_devices() so that events (and devices) are
-       not missed.  For example, if enumeration happened first, there
-       would be no event generated for a device which was attached after
-       enumeration but before monitoring began.
-
-       Note that a filter is added so that we only get events for
-       "hidraw" devices. */
-
-    /* Set up a monitor to monitor hidraw devices */
-    mon = udev_monitor_new_from_netlink(udev, "udev");
-    udev_monitor_filter_add_match_subsystem_devtype(mon, "power_supply", NULL);
-    udev_monitor_enable_receiving(mon);
-    /* Get the file descriptor (fd) for the monitor.
-       This fd will get passed to select() */
-    fd = udev_monitor_get_fd(mon);
-
-
-    /* Create a list of the devices in the 'hidraw' subsystem. */
-    enumerate = udev_enumerate_new(udev);
-    udev_enumerate_add_match_subsystem(enumerate, "power_supply");
-    udev_enumerate_scan_devices(enumerate);
-    devices = udev_enumerate_get_list_entry(enumerate);
-    /* For each item enumerated, print out its information.
-       udev_list_entry_foreach is a macro which expands to
-       a loop. The loop will be executed for each member in
-       devices, setting dev_list_entry to a list entry
-       which contains the device's path in /sys. */
-    udev_list_entry_foreach(dev_list_entry, devices) {
-        const char *path;
-
-        /* Get the filename of the /sys entry for the device
-           and create a udev_device object (dev) representing it */
-        path = udev_list_entry_get_name(dev_list_entry);
-        dev = udev_device_new_from_syspath(udev, path);
-
-        /* usb_device_get_devnode() returns the path to the device node
-           itself in /dev. */
-        printf("Device Node Path: %s\n", udev_device_get_devnode(dev));
-
-        /* The device pointed to by dev contains information about
-           the hidraw device. In order to get information about the
-           USB device, get the parent device with the
-           subsystem/devtype pair of "usb"/"usb_device". This will
-           be several levels up the tree, but the function will find
-           it.*/
-        dev = udev_device_get_parent_with_subsystem_devtype(
-            dev,
-            "usb",
-            "usb_device");
-        if (!dev) {
-            printf("Unable to find parent usb device.");
-            exit(1);
-        }
-
-        /* From here, we can call get_sysattr_value() for each file
-           in the device's /sys entry. The strings passed into these
-           functions (idProduct, idVendor, serial, etc.) correspond
-           directly to the files in the /sys directory which
-           represents the USB device. Note that USB strings are
-           Unicode, UCS2 encoded, but the strings returned from
-           udev_device_get_sysattr_value() are UTF-8 encoded. */
-        printf("  VID/PID: %s %s\n",
-            udev_device_get_sysattr_value(dev,"idVendor"),
-            udev_device_get_sysattr_value(dev, "idProduct"));
-        printf("  %s\n  %s\n",
-            udev_device_get_sysattr_value(dev,"manufacturer"),
-            udev_device_get_sysattr_value(dev,"product"));
-        printf("  serial: %s\n",
-            udev_device_get_sysattr_value(dev, "serial"));
-        udev_device_unref(dev);
-    }
-    /* Free the enumerator object */
-    udev_enumerate_unref(enumerate);
-
-    return 0;
-
-    /* Begin polling for udev events. Events occur when devices
-       attached to the system are added, removed, or change state.
-       udev_monitor_receive_device() will return a device
-       object representing the device which changed and what type of
-       change occured.
-
-       The select() system call is used to ensure that the call to
-       udev_monitor_receive_device() will not block.
-
-       The monitor was set up earler in this file, and monitoring is
-       already underway.
-
-       This section will run continuously, calling usleep() at the end
-       of each pass. This is to demonstrate how to use a udev_monitor
-       in a non-blocking way. */
-    while (1) {
-        /* Set up the call to select(). In this case, select() will
-           only operate on a single file descriptor, the one
-           associated with our udev_monitor. Note that the timeval
-           object is set to 0, which will cause select() to not
-           block. */
-        fd_set fds;
-        struct timeval tv;
-        int ret;
-
-        FD_ZERO(&fds);
-        FD_SET(fd, &fds);
-        tv.tv_sec = 0;
-        tv.tv_usec = 0;
-
-        ret = select(fd+1, &fds, NULL, NULL, &tv);
-
-        /* Check if our file descriptor has received data. */
-        if (ret > 0 && FD_ISSET(fd, &fds)) {
-            printf("\nselect() says there should be data\n");
-
-            /* Make the call to receive the device.
-               select() ensured that this will not block. */
-            dev = udev_monitor_receive_device(mon);
-            if (dev) {
-                printf("Got Device\n");
-                printf("   Node: %s\n", udev_device_get_devnode(dev));
-                printf("   Subsystem: %s\n", udev_device_get_subsystem(dev));
-                printf("   Devtype: %s\n", udev_device_get_devtype(dev));
-
-                printf("   Action: %s\n", udev_device_get_action(dev));
-                udev_device_unref(dev);
-            }
-            else {
-                printf("No Device from receive_device(). An error occured.\n");
-            }
-        }
-        usleep(250*1000);
-        printf(".");
-        fflush(stdout);
-    }
+    printf("Scanning complete...\n");
 
 
     udev_unref(udev);
 
     return 0;
 }
-#endif
